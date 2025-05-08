ce1bf0aa0b1f9b9e8beb0916e266ac48dcf65aa1
LAURA ARBOLEDA GALLEGO <118035967+lauraarboledag@users.noreply.github.com>
2025-05-08 23:37:22 +0000

    Creacion del CSV

--- src/proyecto_integrado_V/collector.py
+++ src/proyecto_integrado_V/collector.py
@@ -17,7 +17,7 @@ class Collector:
         try:
             df = pd.DataFrame()
             headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+                'User-Agent': 'Mozilla/5.0'
             }
 
             response = requests.get(self.url, headers=headers)
